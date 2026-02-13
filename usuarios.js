59a7651d8c8e7697efc523f5a8281ae40a1dd157
espacocuraecuidado <espacocuraecuidado@gmail.com>
2026-02-13 18:19:56 -0300

    Create usuarios.js

---
+++ usuarios.js
@@ -0,0 +1,9 @@
+/* Banco de Dados de Usuários */
+const db_usuarios = [
+    {
+        nomeCompleto: "Administrador",
+        usuario: "admin",
+        senha: "123",
+        email: "admin@curaecuidado.com"
+    }
+];
